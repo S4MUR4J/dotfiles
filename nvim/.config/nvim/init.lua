e9e614e187112f6656f1bf02155c67e2b0c75fed
S4MUR4J <mucha.maciej2000@gmail.com>
2024-11-29 21:52:34 +0100

    Add neovim configuration

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,18 @@
+-- TODO Add debugging and clean a look of nvim
+-- https://www.youtube.com/playlist?list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("vim-options")
+require("lazy").setup("plugins")
---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set scrolloff=5")
+
+vim.cmd("set relativenumber")
+vim.cmd("set nu rnu")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
